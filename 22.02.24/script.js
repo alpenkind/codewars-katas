87521ccf4a9cfd01ca527c4701a5ae9e2137ba84
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-02-22 20:09:25 +0100

    Task4:Counting-Sheep-Kata

--- 22.02.24/script.js
+++ 22.02.24/script.js
@@ -46,3 +46,12 @@ function positiveSum(arr) {
 
   return summe.reduce(sumUp, 0);
 }
+
+//TASK4
+
+/*Consider an array/list of sheep where some sheep may be missing from their place.
+ We need a function that counts the number of sheep present in the array (true means present).*/
+
+function countSheeps(sheep) {
+  return sheep.filter(Boolean).length;
+}
