9d13015fe4ca491bfd37c9e3a1c5f7a8e4ca2a5a
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-02-22 20:01:11 +0100

    Task1-String-to-Number

---
+++ 22.02.24/script.js
@@ -0,0 +1,26 @@
+//TASK 1
+
+//We need a function that can transform a string into a number.
+
+const stringToNumber = function (str) {
+  return parseFloat(str);
+  return null;
+};
+
+console.log(stringToNumber("4"));
+
+//TASK 2
+/* Return negative Number
+
+
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?*/
+
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else if (num <= 0) {
+    return num;
+  }
+}
+
+console.log(makeNegative(7));
