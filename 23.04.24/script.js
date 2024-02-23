b5871688a748df3a19599ea4fa78aad0d4ae160f
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-02-23 16:16:21 +0100

    Task3:Pick-middle-charakters

--- 23.04.24/script.js
+++ 23.04.24/script.js
@@ -19,3 +19,20 @@ function evenNumbers(array, number) {
   const evenArray = array.filter(isEven).slice(-number);
   return evenArray;
 }
+
+//TASK3
+/*You are going to be given a word. Your job is to return the middle character of the word.
+   If the word's length is odd, return the middle character.
+   If the word's length is even, return the middle 2 characters.*/
+
+let position = 0;
+
+function getMiddle(s) {
+  if (s.length % 2 !== 0) {
+    position = s.length / 2;
+    return s.substring(position, position + 1);
+  } else {
+    position = s.length / 2;
+    return s.substring(position - 1, position + 1);
+  }
+}
