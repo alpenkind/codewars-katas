abf746757493fc8cfc16b403e04d32adf4a6f206
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-02-26 18:27:43 +0100

    Task2:Cat-years & Dog-years

--- 26.02.2024/script.js
+++ 26.02.2024/script.js
@@ -10,14 +10,56 @@ Input: 145263 Output: 654321
 Input: 123456789 Output: 987654321  */
 
 function descendingOrder(n) {
-  let neu = n
+  let arr = n
     .toString()
     .split("")
     .map((str) => parseInt(str));
 
-  let sortedarray = neu.sort(function (a, b) {
+  let sortedarray = arr.sort(function (a, b) {
     return b - a;
   });
 
   return parseInt(sortedarray.join(""));
 }
+
+//Task 2
+// Cat Years & Dog Years
+
+/*
+I have a cat and a dog.
+
+
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+
+NOTES:
+
+
+humanYears >= 1
+humanYears are whole numbers only
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+*/
+
+let humanYearsCatYearsDogYears = function (humanYears) {
+  if (humanYears === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (humanYears === 2) {
+    catYears = 15 + 9;
+    dogYears = 15 + 9;
+  } else {
+    catYears = 15 + 9 + (humanYears - 2) * 4;
+    dogYears = 15 + 9 + (humanYears - 2) * 5;
+  }
+  return [humanYears, catYears, dogYears];
+};
